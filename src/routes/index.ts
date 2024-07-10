396c7d898c9e724ae4dfa9471e40619f7f8c0829
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-10 17:01:57 -0300

    recebendo dados na requisição

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 
 import productsRouter from "./products";
+import flightsRouter from "./flights";
 import { middleware } from "../middlewares/middleware";
 
 const router = express.Router();
 
-router.use(middleware); //USANDO EM TODAS AS ROTAS
+//router.use(middleware); //USANDO EM TODAS AS ROTAS
 
 router.use("/products", productsRouter);
+router.use("/flights", flightsRouter);
 
 router.get("/", (req, res) => {
   let name = "a";
