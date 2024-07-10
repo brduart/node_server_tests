c40a942dd35c65d9e73a79d47f4f9e58f317c613
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-10 10:48:27 -0300

    criação basica de rotas

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,16 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, "../public")));
 
+server.get("/ping", (req, res) => {
+  res.json({ ping: true });
+});
+
+server.get("/ping/:id", (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  res.json({ ping: true });
+});
+
 server.get("/", (req, res) => {
   let name = "a";
   let lastName = "b";
