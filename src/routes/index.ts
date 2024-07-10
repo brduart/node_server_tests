fd380fc908bd38a97c97e8ec90177b384ac514e1
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-10 11:02:37 -0300

    outra forma de organizar rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
+import productsRouter from "./products";
+
 const router = express.Router();
+
+router.use("/products", productsRouter);
 
 router.get("/", (req, res) => {
   let name = "a";
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import helmet from "helmet";
 import path from "path";
 
 import router from "./routes";
-import productsRouter from "./routes/products";
 
 const server = express();
 
@@ -12,8 +11,6 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, "../public")));
 
-server.use("/products", productsRouter);
-//server.use("/items", itemsRouter);
 server.use("/", router);
 
 server.listen(3000, () => {
