80ee097c7c1dae971fdff43b7e20b4ff16d6bc21
Bruno Duarte <bruno.duart98@gmail.com>
2024-07-10 10:12:03 -0300

    servidor básico com node

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
-console.log("hello");
-console.log(`${process.env.TESTE_ENV}`);
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+  res.end("Hello!");
+});
+
+server.listen(3000, () => {
+  console.log("server ok");
+});
